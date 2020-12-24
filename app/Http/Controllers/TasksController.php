1a4a85573a2b1fe722b5e9c0416d8ecafabf099d
Renato Alexandre <renatoacf@gmail.com>
2020-12-24 07:42:08 -0300

    Model e Controller Task

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    
+    public function index()
+    {
+         
+        return Task::all();
+        
+    }
+
+    public function store(Request $request)
+    {
+
+        $task = Task::create([
+            'name'=>$request->input('name'),
+            'complete'=>$request->input('complete')
+        ]);
+
+        return $task;
+    }
+
+}
