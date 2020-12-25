c85c5bcd140ab3c7bb48328ea94ddd8b8275fa42
Renato Alexandre <renatoacf@gmail.com>
2020-12-25 00:34:54 -0300

    Trabalhando com migration e factory

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Task extends Model
 {
 
+    use HasFactory;
+    
     public $timestamps = false;
 
     protected $fillable = ['name', 'complete'];
